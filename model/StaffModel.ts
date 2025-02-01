f8fcd3735bbeda094bc6a21d80a7171d71833fa0
DonShanilka <shanilka799@gmail.com>
2025-02-01 22:50:32 +0530

    create StaffModel class

---
+++ model/StaffModel.ts
@@ -0,0 +1,18 @@
+export class StaffModel {
+  id!: String;
+  firstName!: String;
+  lastName!: String;
+  designation!: String;
+  gender!: String;
+  joinedDate!: Date;
+  dateOfBirth!: Date;
+  mobile!: String;
+  email!: String;
+  role!: String;
+  address1!: String;
+  address2!: String;
+  address3!: String;
+  address4!: String;
+  address5!: String;
+  fieldId!: String;
+}
