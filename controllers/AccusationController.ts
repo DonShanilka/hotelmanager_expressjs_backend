1b28aef97b390df7b9e11f026785e0a9266caace
Shanilka800@# <shanilka799@gmail.com>
2025-02-07 14:53:51 +0530

    create updateAccusation in AccusationController

--- controllers/AccusationController.ts
+++ controllers/AccusationController.ts
@@ -1,20 +1,30 @@
 import express from 'express';
 import { Accusation } from '../model/Accusation';
-import { AccusationAdd } from '../services/AccusationService';
+import { AccusationAdd, AccusationUpdate } from '../services/AccusationService';
 
 const router = express.Router();
 
-exports.saveAccusation = async(req : any, res : any) => {
+exports.saveAccusation = async (req : any, res : any) => {
   const accusation = req.body;
-  console.log("AccusationL: ", accusation);
+  console.log("Accusation: ", accusation);
 
   try {
     const addAccusation = await AccusationAdd(accusation);
     res.send(200).json(addAccusation);
   } catch (error) {
     console.error("Erro Adding Accusation: ", error);
-
   }
 }
 
+exports.updateAccusation = async (req : any, res : any) => {
+  const id = req.params.accusationId;
+  console.log("Accusation Id For Update: ", id);
+  const accusation : Accusation = req.body;
 
+  try {
+    await AccusationUpdate(id, accusation);
+    res.send("Accusation Updated");
+  } catch (error) {
+    console.log("Error Update Accusation: ", error);
+  }
+}
