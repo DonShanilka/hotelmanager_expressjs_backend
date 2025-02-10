f9ae19e1cc561a69c4bfc02e6a0441df9179c0cf
Shanilka800@# <shanilka799@gmail.com>
2025-02-10 13:50:16 +0530

    cerate updateRoom funtipn in conrtroller class

--- controllers/RoomController.ts
+++ controllers/RoomController.ts
@@ -2,51 +2,58 @@ import multer from 'multer';
 import { prisma } from '../db/Prisma_data_storage';
 import { Room } from '../model/Room';
 import { Request, Response } from 'express';
+import { RoomUpdate } from '../services/RoomService';
 
-// Set up multer for file uploads
-const storage = multer.memoryStorage(); // Store file in memory (Buffer)
-const upload = multer({ storage: storage }); // Use multer middleware
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage }); 
 
-// Function to save the room
-export async function saveRoom(req: Request, res: Response) {
+
+exports.saveRoom = async (req: Request, res: Response) => {
   try {
-    // Use multer middleware to handle file upload and parse form data
-    // 'selectedImage' is the name of the file input field in the HTML form
+    
     upload.single('selectedImage')(req, res, async (err) => {
       if (err) {
         return res.status(400).json({ error: "File upload error" });
       }
 
-      // Access form data
       const { roomNumber, roomType, hallFloor, price, status } = req.body;
-      const selectedImage = req.file ? req.file.buffer : Buffer.alloc(0); // If file uploaded, get the buffer, else empty buffer
-
-      // Validate form data
+      const selectedImage = req.file ? req.file.buffer : Buffer.alloc(0); // If file 
+      
       if (!roomNumber || !roomType || !hallFloor || !price || !status) {
         return res.status(400).json({ error: "Missing required fields" });
       }
-
-      // Prepare room data
+      
       const roomData: Room = {
-        roomNumber: roomNumber,  // Assuming roomNumber is a string and needs to be parsed
+        roomNumber: roomNumber, 
         roomType,
         selectedImage,
-        hallFloor: parseInt(hallFloor),   // Convert floor to number if needed
-        price: parseFloat(price),         // Convert price to float
+        hallFloor: parseInt(hallFloor),   
+        price: parseFloat(price),         
         status,
       };
 
-      // Create a new room in the database
       const newRoom = await prisma.room.create({
         data: roomData,
       });
 
       console.log("Room Added Successfully:", newRoom);
-      return res.status(201).json(newRoom); // Return the created room as a response
+      return res.status(201).json(newRoom); 
     });
 
   } catch (error) {
     console.error("Error Adding Room:", error);
     return res.status(500).json({ error: "Failed to add room" });
+  }
+}
+
+exports.updateRoom = async (req : any, res : any) => {
+  const id = req.params.id;
+  const room : Room = req.body;
+
+  try {
+    await RoomUpdate(id, room);
+    res.send("Room Update");
+  } catch (error) {
+    console.log("Error Updating Room: ", error);
   }
 }
