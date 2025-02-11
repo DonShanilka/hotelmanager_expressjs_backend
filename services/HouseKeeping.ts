beb8256e410630d18193b76aac04ea080e8cc63d
Shanilka800@# <shanilka799@gmail.com>
2025-02-11 22:31:48 +0530

    create HouseKeepingUpdate funtion

--- services/HouseKeeping.ts
+++ services/HouseKeeping.ts
@@ -18,4 +18,32 @@ export async function HouseKeepingAdd(houseKeeping: HouseKeeping) {
     console.error('Error adding HouseKeeping:', error);
     throw error;
   }
+};
+
+export async function HouseKeepingUpdate(id: number, houseKeeping : HouseKeeping) {
+  try {
+
+    const existingHouseKeeping = await prisma.houseKeeping.findUnique({
+      where: {houseKeepingId: id },
+    });
+
+    if (!existingHouseKeeping) {
+      throw new Error(`HouseKeeping with ID ${id} not found`);
+    }
+
+    const houseKeepingUpdate = await prisma.houseKeeping.update({
+      where: {houseKeepingId : id},
+      data : {
+        roomNumber : houseKeeping.roomNumber,
+        cleaningDate : houseKeeping.cleaningDate,
+        status : houseKeeping.status,
+        specialTasks : houseKeeping.specialTasks,
+        empId : houseKeeping.empId
+      }
+    })
+    // alert("Success Fully Updated Guest: ");
+    console.log("Success Fully Updated HouseKeeping: ", houseKeepingUpdate);
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
