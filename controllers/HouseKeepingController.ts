551f6cb31484a97cd31a2c512f9516d53de3b86b
Shanilka800@# <shanilka799@gmail.com>
2025-02-11 22:49:47 +0530

    create saveHouseKeeping

---
+++ controllers/HouseKeepingController.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { HouseKeepingAdd } from '../services/HouseKeeping';
+
+const router = express.Router();
+
+exports.saveHouseKeeping = async (req : any, res : any) => {
+  const houseKeeping = req.body;
+  console.log("Received HouseKeeping: ", houseKeeping);
+
+  try {
+    const addHouseKeeping = await HouseKeepingAdd(houseKeeping);
+    res.status(200).json(addHouseKeeping);
+  } catch (error) {
+    console.error("Error Adding HouseKeeping: ", error);
+    res.status(400).send("Error Adding HouseKeeping")
+  }
+};
