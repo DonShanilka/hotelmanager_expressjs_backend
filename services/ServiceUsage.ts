f49ca46bc5b5ea942c5134ec2a68f8b8077ac6d9
Shanilka800@# <shanilka799@gmail.com>
2025-02-12 10:56:22 +0530

    create UsageDelete

--- services/ServiceUsage.ts
+++ services/ServiceUsage.ts
@@ -2,7 +2,7 @@ import { prisma } from '../db/Prisma_data_storage';
 import { ServiceUsage } from '../model/ServiceUsage';
 
 
-export async function ServiceUsageAdd(serviceUsage : ServiceUsage) {
+export async function UsageAdd(serviceUsage : ServiceUsage) {
   try {
     const newService = await prisma.serviceUsage.create({
       data: {
@@ -20,5 +20,52 @@ export async function ServiceUsageAdd(serviceUsage : ServiceUsage) {
   } catch (error) {
     console.error("Error Adding ServiceUsage:", error);
     throw new Error("Failed to add ServiceUsage"); 
+  }
+};
+
+export async function UsageUpdate(id : number, serviceUsage : ServiceUsage) {
+  try {
+    const existingUsage = await prisma.serviceUsage.findUnique({
+      where : {usageID : id},
+    });
+
+    if (!existingUsage) {
+      throw new Error(`${id} Usage ID not found`);
+    }
+
+    const serviceUpdate = await prisma.serviceUsage.update({
+      where: {usageID : id},
+      data : {
+        bookingID : serviceUsage.bookingID,
+        guestId : serviceUsage.guestId,
+        serviceID : serviceUsage.serviceID,
+        quantity : serviceUsage.quantity,
+        totalCost : serviceUsage.totalCost,
+        usageDate : serviceUsage.usageDate
+      }
+    });
+    console.log("Success Full Update Usage", serviceUpdate);
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+export async function UsageDelete(id : number) {
+  try {
+    const existingUsage = await prisma.serviceUsage.findUnique({
+      where : {usageID : id}
+    });
+
+    if (!existingUsage) {
+      throw new Error(`${id} Usage ID not found`);
+    }
+
+    await prisma.serviceUsage.delete({
+      where : {usageID : id}
+    });
+    
+    console.log("Success Fully Deleted Usage", id);
+  } catch (error) {
+    console.log("Error", error);
   }
 }
