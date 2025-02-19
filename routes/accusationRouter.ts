649f76ab128199f7376544456f834b04769a7a98
DonShanilka <shanilka799@gmail.com>
2025-02-19 16:21:16 +0530

    update

--- routes/accusationRouter.ts
+++ routes/accusationRouter.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const router = express.Router();
 const accusationController = require('../controllers/AccusationController');
 
-router.post('/saveAccusation', accusationController.saveAccusation);
-router.put('/updateAccusation/:id', accusationController.updateAccusation);
-router.delete('/deleteAccusation/:id', accusationController.deleteAccusation);
-router.get('/getAllAccusation', accusationController.getAllAccusation);
+router.post('/acc/saveAccusation', accusationController.saveAccusation);
+router.put('acc//updateAccusation/:id', accusationController.updateAccusation);
+router.delete('/acc/deleteAccusation/:id', accusationController.deleteAccusation);
+router.get('/acc/getAllAccusation', accusationController.getAllAccusation);
 
 module.exports = router;
--- routes/roomRouter.ts
+++ routes/roomRouter.ts
@@ -3,9 +3,9 @@ import express from 'express';
 const router = express.Router();
 const roomController = require('../controllers/RoomController');
 
-router.post('/saveRoom', roomController.saveRoom); 
-router.put('/updateRoom/:id', roomController.updateRoom);
-router.delete('/deleteRoom/:id', roomController.deletRoom);
-router.get('/getAllRoom', roomController.getAllRooms);
+router.post('/room/saveRoom', roomController.saveRoom);
+router.put('/room/updateRoom/:id', roomController.updateRoom);
+router.delete('/room/deleteRoom/:id', roomController.deletRoom);
+router.get('/room/getAllRoom', roomController.getAllRooms);
 
 module.exports = router;
